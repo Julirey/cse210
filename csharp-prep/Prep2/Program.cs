aa3c0bf57e2d17e9c9d4bca035ccf56259105cd3
julioreyes27 <rey23009@byui.edu>
2023-09-28 05:52:31 -0400

    Finished prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int gradePercentageInt = int.Parse(gradePercentage);
+
+        string gradeLetter = "";
+
+        if (gradePercentageInt >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (gradePercentageInt >= 80) 
+        {
+            gradeLetter = "B";
+        }
+        else if (gradePercentageInt >= 70) 
+        {
+            gradeLetter = "C";
+        }
+        else if (gradePercentageInt >= 60) 
+        {
+            gradeLetter = "D";
+        }
+        else 
+        {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {gradeLetter}");
+
+        if (gradePercentageInt >= 70)
+        {
+            Console.WriteLine("You have passed, congrats!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass, don't give up yet!");
+            Console.WriteLine("You can always try next time!");
+        }
     }
 }
