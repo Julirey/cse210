e23debd87abfbbff2f8c423b35d3cbc9c0076d85
julioreyes27 <rey23009@byui.edu>
2023-09-28 08:18:49 -0400

    Finished prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Data;
+using System.Diagnostics.Tracing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string answer = "";
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guessNumber = -1;
+        
+        
+        while (magicNumber != guessNumber)
+        {
+            Console.Write("What is your guess? ");
+            answer = Console.ReadLine();
+            guessNumber = int.Parse(answer);
+            
+            if (magicNumber > guessNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guessNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+        
+        Console.Write("You guessed it!");
     }
 }
