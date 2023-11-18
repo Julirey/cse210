fa645dcb4b4e6137392f43a3c1723954ffec0d73
julioreyes27 <reyes.0271204@gmail.com>
2023-11-18 18:10:48 -0400

    Add small conditional gate

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -33,38 +33,41 @@ class Program
                 Console.Write("Which type of goal would you like to create: ");
                 choice = Console.ReadLine();
 
-                Console.Write("What is the name of your goal? ");
-                name = Console.ReadLine();
-                Console.Write("What is a short description of it? ");
-                description = Console.ReadLine();
-                Console.Write("What is the amount of points associated with this goal? ");
-                points = int.Parse(Console.ReadLine());
-                
-                if (choice == "1")
+                if (choice == "1" || choice == "2" || choice == "3")
                 {
-                    Simple simple = new Simple(name, description, points);
-                    journal.AddGoal(simple);
-                }
-                else if (choice == "2")
-                {
-                    Eternal eternal = new Eternal(name, description, points);
-                    journal.AddGoal(eternal);
-                }
-                else if (choice == "3")
-                {   
-                    Console.Write("How many times does this goal need to be accomplished for a bonus? ");
-                    timesMax = int.Parse(Console.ReadLine());
-                    Console.Write("What is the bonus for accomplishing it that many times? ");
-                    bonus = int.Parse(Console.ReadLine());
+                    Console.Write("What is the name of your goal? ");
+                    name = Console.ReadLine();
+                    Console.Write("What is a short description of it? ");
+                    description = Console.ReadLine();
+                    Console.Write("What is the amount of points associated with this goal? ");
+                    points = int.Parse(Console.ReadLine());
+                    
+                    if (choice == "1")
+                    {
+                        Simple simple = new Simple(name, description, points);
+                        journal.AddGoal(simple);
+                    }
+                    else if (choice == "2")
+                    {
+                        Eternal eternal = new Eternal(name, description, points);
+                        journal.AddGoal(eternal);
+                    }
+                    else if (choice == "3")
+                    {   
+                        Console.Write("How many times does this goal need to be accomplished for a bonus? ");
+                        timesMax = int.Parse(Console.ReadLine());
+                        Console.Write("What is the bonus for accomplishing it that many times? ");
+                        bonus = int.Parse(Console.ReadLine());
 
-                    Checklist checklist = new Checklist(name, description, points, bonus, timesMax);
-                    journal.AddGoal(checklist);
+                        Checklist checklist = new Checklist(name, description, points, bonus, timesMax);
+                        journal.AddGoal(checklist);
+                    }
                 }
                 else
                 {
                     Console.Write("That is not a valid option. Press Enter to go back to the menu ");
                     Console.ReadLine();
-                }
+                }  
             }
             // List Goals
             else if (choice == "2")
